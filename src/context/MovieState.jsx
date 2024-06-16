282e64d7be2273d4f76d7628b6cb1a144992ff2e
rinkuJat <jatr26008@gmail.com>
2024-06-17 00:03:24 +0530

    Some changes made

--- src/context/MovieState.jsx
+++ src/context/MovieState.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'; // Import PropTypes
 import MovieContext from './movieContext';
 
 const MovieState = (props) => {
-    const host = "https://book-my-show-backend-virid.vercel.app/";
+    const host = "https://book-my-show-backend-virid.vercel.app";
     
     const MoviesInitial = [];
     const [Movies, setMovies] = useState(MoviesInitial);
